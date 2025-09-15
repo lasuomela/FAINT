1a584eac09e358d85747de5046a291e6a5b7dbbd
Lauri Suomela <lauri.a.suomela@tuni.fi>
2025-09-15 16:33:17 +0300

    Cleanup

--- slurm_tools/dl_offlinevint_puhti_weights.sh
+++
@@ -1,9 +0,0 @@
-# bin/bash
-
-# Get the run id from arguments
-run_id=$1
-echo "Downloading weights for run id: ${run_id}"
-
-# Create the destination directory if it doesn't exist
-mkdir -p faint/deployment/src/faint_deployment/model_weights/${run_id}/
-scp suomelal@puhti.csc.fi:/scratch/project_2010179/Theiavint_Offline/train/logs/DepthGoals/${run_id}/*.pt faint/deployment/src/faint_deployment/model_weights/${run_id}/
